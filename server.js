973894467c0d1879937cd3ff3763e34c25eb7dce
surajgurung <microbiosuraj@gmail.com>
2020-07-28 09:09:32 -0400

    edit on server.js for mongo

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = (process.env.PORT || 3000);
+ const PORT = (process.env.PORT || 3000);
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -15,10 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(MONGODB_URI); 
 
 // routes
 app.use(require("./routes/api.js"));
